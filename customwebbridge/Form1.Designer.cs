da04f6c00571e960f1796de14094ea6f5717e9db
Iulian Breahna <breahnai@poloagroindustriale.edu.it>
2024-02-28 11:00:44 +0100

    Progetto con un tasto per prova stampante

---
+++ customwebbridge/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace customwebbridge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_usb = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bt_usb
+            // 
+            this.bt_usb.Location = new System.Drawing.Point(344, 172);
+            this.bt_usb.Name = "bt_usb";
+            this.bt_usb.Size = new System.Drawing.Size(108, 68);
+            this.bt_usb.TabIndex = 0;
+            this.bt_usb.Text = "USB";
+            this.bt_usb.UseVisualStyleBackColor = true;
+            this.bt_usb.Click += new System.EventHandler(this.bt_usb_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bt_usb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_usb;
+    }
+}
+
---
+++ customwebbridge/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Custom.CuCustomWndAPI;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace customwebbridge
+{
+    public partial class Form1 : Form
+    {
+        CuCustomWndAPIWrap customWndAPIWrap = null;
+        CuCustomWndDevice dev = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_usb_Click(object sender, EventArgs e)
+        {
+            USBDevice[] usbArray = customWndAPIWrap.EnumUSBDevices();
+            String[] strusbArray = new String[usbArray.Length];
+
+            for (int i = 0; i < usbArray.Length; i++)
+            {
+                USBDevice u = usbArray[i];
+                strusbArray[i] = u.SerialNumber;
+            }
+
+            if (usbArray.Length > 0)
+            {
+                try
+                {
+                    dev = customWndAPIWrap.OpenPrinterUSB(usbArray[0]);
+                    MessageBox.Show("USB device paired.");
+                    
+                    
+                    String strTextToPrint = "Marco";
+
+                    PrintFontSettings pfs = new PrintFontSettings();
+                    try
+                    {
+                        //Exec the print of the text
+                        dev.PrintText(strTextToPrint, pfs, true);
+                    }
+                    catch (Exception ex)
+                    {
+                        ShowErrorMessage(ex);
+                    }
+                    try
+                    {
+                        //Exec Partial Cut
+                        dev.Cut(CuCustomWndDevice.CutType.CUT_PARTIAL);
+                    }
+                    catch (Exception ex)
+                    {
+                        ShowErrorMessage(ex);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ShowErrorMessage(ex);
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("No USB devices found.");
+            }
+        }
+        private void ShowErrorMessage(Exception ex)
+        {
+            String strErrorDescription = "";
+            String strErrorOrigin = "";
+
+            if (ex.GetType() == typeof(CuCustomWndAPIWrapException))
+            {
+                strErrorOrigin = "CuCustomWndAPIWrapException Error";
+                strErrorDescription = ((CuCustomWndAPIWrapException)ex).ErrorDescription;
+            }
+            else
+            {
+                strErrorOrigin = "Exception Error";
+                strErrorDescription = ex.ToString();
+            }
+
+            //Show the messagebox
+            MessageBox.Show(strErrorDescription, strErrorOrigin, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                //Create the Class
+                customWndAPIWrap = new CuCustomWndAPIWrap(CuCustomWndAPIWrap.CcwLogVerbosity.CCW_LOG_DEEP_DEBUG, null);
+                //Init the library
+                customWndAPIWrap.InitLibrary();
+            }
+            catch (Exception ex)
+            {
+                ShowErrorMessage(ex);
+                return;
+            }
+        }
+    }
+}
